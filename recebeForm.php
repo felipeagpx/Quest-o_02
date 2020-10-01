1a36a157664dbb8aab424c35f6dd227c1d8b19f8
felipeagpx <69019342+felipeagpx@users.noreply.github.com>
2020-10-01 19:55:17 -0300

    Add files via upload

---
+++ recebeForm.php
@@ -0,0 +1,9 @@
+<?php
+
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$datanasc = $_POST ['datanasc'];
+
+echo("O nome digitado foi: $nome".'<br>');
+echo("O e-mail digitado foi: $email".'<br>');
+echo("A data de nascimento digitada foi: $datanasc".'<br>');
